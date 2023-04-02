3699dd2fc86d251444743272a77a2222c4be53d7
renan-unisociesc <128191914+renan-unisociesc@users.noreply.github.com>
2023-04-02 16:12:04 -0300

    resolucao
    resolucao do primeiro exercicio

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -4,6 +4,8 @@
  */
 package javaapplication4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Renan
@@ -14,7 +16,23 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int valor = 1;
+        int somaPares =0;
+        int produtoImpares = 1;
+        Scanner entrada = new Scanner (System.in);
+        while (valor > 0) {
+        System.out.println("digite um valor inteiro positivo: ");
+        valor = entrada.nextInt();
+            if (valor > 0) {
+                if ((valor % 2) ==0 ){
+                somaPares = somaPares + valor;
+                } else {
+                produtoImpares = produtoImpares * valor;
+                }
+            }
+        }
+    System.out.println("A soma dos numeros pares e " + somaPares);
+    System.out.println("O produto dos numeros impares e " + produtoImpares);    
     }
     
 }
